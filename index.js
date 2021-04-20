83a739313779d5fd2559fe92b6131912c7c5281b
CaioMoriUTFPR <caiomori@alunos.utfpr.edu.br>
2021-04-19 21:55:16 -0300

    Adicionado, função para preencher item na lista

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-/* eslint-disable max-len */
 const express = require('express');
 const app = express();
 const porta = 80;
 
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
 
 app.get('/', (req, res) => {
-  const hoje = new Date();
-  const opcoes = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
-  const dia = hoje.toLocaleDateString('pt-BR', opcoes);
-  res.render('index', {dia: dia});
+  res.render('index', {dia: dia, itens: null});
+});
+
+app.post('/', (req, res) => {
+  const itens = req.body.item;
+  res.render('index', {dia: dia, itens: itens});
 });
 
 app.listen(porta, () => {
   console.log(`Servidor rodando na porta ${porta}`);
 });
+
+const hoje = new Date();
+const opcoes = {
+  weekday: 'long',
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+};
+const dia = hoje.toLocaleDateString('pt-BR', opcoes);
