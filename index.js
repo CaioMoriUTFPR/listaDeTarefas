0818b37c3c0bc27c5b3eed9121868d109b4c92bf
CaioMoriUTFPR <caiomori@alunos.utfpr.edu.br>
2021-04-19 21:41:04 -0300

    Retirado o teste de cores no css

--- index.js
+++ index.js
@@ -7,12 +7,9 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   const hoje = new Date();
-  const idDiaAtual = hoje.getDay();
-  const listaCores = ['Green', 'Blue', 'Purple', 'Gray', 'Brown', 'Yellow', 'Red'];
-  const corDia = listaCores[idDiaAtual];
   const opcoes = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
   const dia = hoje.toLocaleDateString('pt-BR', opcoes);
-  res.render('index', {dia: dia, corDia: corDia});
+  res.render('index', {dia: dia});
 });
 
 app.listen(porta, () => {
