71e3adc4bcd18654f44e698139269f68f1024909
CaioMoriUTFPR <caiomori@alunos.utfpr.edu.br>
2021-04-19 21:38:24 -0300

    Melhorando mais ainda a data obtida

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 const express = require('express');
 const app = express();
 const porta = 80;
@@ -6,13 +7,11 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
   const hoje = new Date();
-  const diaAtual = hoje.getDay();
-  // eslint-disable-next-line max-len
-  const listaDias = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
-  // eslint-disable-next-line max-len
+  const idDiaAtual = hoje.getDay();
   const listaCores = ['Green', 'Blue', 'Purple', 'Gray', 'Brown', 'Yellow', 'Red'];
-  const dia = listaDias[diaAtual];
-  const corDia = listaCores[diaAtual];
+  const corDia = listaCores[idDiaAtual];
+  const opcoes = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
+  const dia = hoje.toLocaleDateString('pt-BR', opcoes);
   res.render('index', {dia: dia, corDia: corDia});
 });
 
